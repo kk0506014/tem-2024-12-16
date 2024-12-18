bbd5c8c3cd64190059e3c2f3373106793a4449cb
kk0506014 <kk0506014@gmail.com>
2024-12-18 11:21:04 +0900

    27 class

--- src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/tem/domain/post/post/controller/PostController.java
@@ -1,62 +1,75 @@
 package com.ll.tem.domain.post.post.controller;
 
+import com.ll.tem.domain.post.post.entity.Post;
+import com.ll.tem.domain.post.post.repository.PostService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/posts")
+@RequiredArgsConstructor
 public class PostController {
-    private String getFormHtml(String errorMessage, String title, String content) {
-        return """
-                <div>%s</div>
-                <form method="POST">
-                    <input type="text" name="title" placeholder="제목" value="%s">
-                    <br>
-                    <textarea name="content" placeholder="내용">%s</textarea>
-                    <br>
-                    <button type="submit">글쓰기</button>
-                </form>
-                """.formatted(errorMessage, title, content);
+    private final PostService postService;
+
+    @GetMapping
+    public String showList(Model model) {
+        List<Post> posts = postService.findAllByOrderByIdDesc();
+        model.addAttribute("posts", posts);
+
+        return "domain/post/post/list";
+    }
+
+
+    @GetMapping("/{id}")
+    public String showDetail(Model model, @PathVariable long id) {
+        Post post = postService.findById(id).get();
+
+        model.addAttribute("post", post);
+
+        return "domain/post/post/detail";
+    }
+
+
+    private record PostWriteForm(
+            @NotBlank(message = "01-제목을 입력해주세요.")
+            @Length(min = 2, message = "02-제목을 2자 이상 입력해주세요.")
+            String title,
+            @NotBlank(message = "03-내용을 입력해주세요.")
+            @Length(min = 2, message = "04-내용을 2자 이상 입력해주세요.")
+            String content
+    ) {
     }
 
     @GetMapping("/write")
-    @ResponseBody
-    public String showWrite() {
-        return getFormHtml("", "", "");
+    public String showWrite(
+            PostWriteForm form
+    ) {
+        return "domain/post/post/write";
     }
 
     @PostMapping("/write")
-    @ResponseBody
     public String write(
-            String title,
-            String content
+            @Valid PostWriteForm form,
+            BindingResult bindingResult,
+            Model model
     ) {
-        if (title == null || title.isBlank()) {
-            return getFormHtml("제목을 입력해주세요.", title, content);
+        if (bindingResult.hasErrors()) {
+            return "domain/post/post/write";
         }
 
-        if (title.length() < 5) {
-            return getFormHtml("제목을 5자 이상 입력해주세요.", title, content);
-        }
+        postService.write(form.title, form.content);
 
-        if (content == null || content.isBlank()) {
-            return getFormHtml("내용을 입력해주세요.", title, content);
-        }
-
-        if (content.length() < 10) {
-            return getFormHtml("내용을 10자 이상 입력해주세요.", title, content);
-        }
-
-        return """
-                <h1>글쓰기 완료</h1>
-                
-                <div>
-                    <h2>%s</h2>
-                    <p>%s</p>
-                </div>
-                """.formatted(title, content);
+        return "redirect:/posts";
     }
 }
